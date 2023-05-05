90be05719d49c6a568b5dc07a8d61375d54f7abd
Tannon Kew <kew@cl.uzh.ch>
2023-05-05 11:05:42 +0200

    medeasi runs

--- run.sh
+++ run.sh
@@ -2,43 +2,93 @@
 # -*- coding: utf-8 -*-
 
 # Example call:
-# nohup bash run.sh > logs/asset_all.jobs 2>&1 &
+# nohup bash run_with_args.sh \
+#   --input_file "resources/data/med-easi/med-easi.test.jsonl" \
+#   --examples "resources/data/med-easi/med-easi.validation.jsonl" \
+#   --model_configs "exp_configs/cluster/bloom-560m exp_configs/cluster/bloom-1b1.json exp_configs/cluster/bloom-3b.json" \
+#   --seeds "489 287 723" \
+#   --prompt_ids "p0 p1 p2" \
+# > logs/medeasi_all.jobs 2>&1 &
 
 # WARNING this will attempt to run all the models with configs defined in the exp_configs/cluster folder!
 
 # Hardcoded paths
 MODEL_CONFIG_DIR=exp_configs/cluster
 DATA_DIR=resources/data
+model_config_files=""
+seeds="489 287 723"
+prompt_ids="p0 p1 p2"
 
-# optional argument to specify which models to run (otherwise will run all models)
-model_config_files=("$@")
+# parse arguments
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    --input_file)
+    input_file="$2"
+    shift 2
+    ;;
+    --examples)
+    examples="$2"
+    shift 2
+    ;;
+    --model_configs)
+    model_config_files="$2"
+    shift 2
+    ;;
+    --seeds)
+    seeds="$2"
+    shift 2
+    ;;
+    --prompt_ids)
+    prompt_ids="$2"
+    shift 2
+    ;;
+    *)
+    echo "Unknown option: $1"
+    exit 1
+    ;;
+  esac
+done
+
+# Convert the space-separated string into an array
+IFS=' ' read -ra model_config_files <<< "$model_config_files"
+IFS=' ' read -ra seeds <<< "$seeds"
+IFS=' ' read -ra prompt_ids <<< "$prompt_ids"
+
+# Now you can use file1, file2, and values_array in your script
+
 
 if [ -z "$model_config_files" ]; then
     echo "No model config files specified, will run all configs in $MODEL_CONFIG_DIR"
-    model_config_files=($(ls $MODEL_CONFIG_DIR/*.json))
+    model_config_files=($(ls $MODEL_CONFIG_DIR/*.json | grep -v "openai-*")) # ignore openai configs!
 fi
 
 echo ""
-echo "Running models: ${model_config_files[@]}"
+echo "Input File: $input_file"
+echo "Examples: $examples"
+echo "Seeds: ${seeds[@]}"
+echo "Prompt IDs: ${prompt_ids[@]}"
+echo "Models: ${model_config_files[@]}"
 echo ""
 
 # iterate through array using a counter
-for prompt_id in p0 p1 p2; do
+for prompt_id in "${prompt_ids[@]}"; do
     echo ""
         echo "### PROMPT ID $prompt_id ###"
     echo ""
-    for seed in 489 287 723; do
+    for seed in "${seeds[@]}"; do
         echo ""
         echo "### SEED $seed ###"
         echo ""
-        for ((i=0; i<${#model_config_files[@]}; i++)); do
+        for model_config_file in "${model_config_files[@]}"; do
             echo ""
-            echo "### $i ${model_config_files[$i]} ###"
+            echo "### $model_config_file ###"
 
-            python -m run "${model_config_files[$i]}" \
+            python -m run "$model_config_file" \
                 --seed $seed \
-                --input_file $DATA_DIR/asset/dataset/asset.test.jsonl \
-                --examples $DATA_DIR/asset/dataset/asset.valid.jsonl \
+                --input_file $input_file \
+                --examples $examples \
                 --prompt_json prompts/$prompt_id.json \
                 --n_refs 1 --few_shot_n 3
         done
